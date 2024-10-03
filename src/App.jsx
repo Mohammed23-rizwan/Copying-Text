4203533bdfaf300717806d87009fdc32b457828b
Mohammed23-rizwan <rizuart23@gmail.com>
2024-10-03 21:40:36 +0530

    fully functional added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { IoCopyOutline } from "react-icons/io5";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [Copy, setCopy] = useState("Default Content");
+
+  const handleCopy = async () => {
+    try {
+      await window.navigator.clipboard.writeText(Copy);
+    } catch (err) {
+      console.error("Unable to copy to clipboard.", err);
+      alert("Copy to clipboard failed.");
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-screen h-screen grid place-items-center">
+      <div className="rounded-[5px] w-[320px] h-[320px] bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)] md:w-[450px] md:h-[320px] lg:w-[550px]">
+        <div className="div w-full flex flex-col items-center">
+          <h2 className="text-center mt-8 text-lg text-blue-300">
+            Copying Text
+          </h2>
+          <textarea
+            name=""
+            id=""
+            onChange={(e) => {
+              setCopy(e.target.value);
+            }}
+            placeholder="Enter a text to voice"
+            className="text-blue-300 resize-none rounded-[5px] p-2 w-[85%] h-[150px] border border-blue-300 outline-none mt-[10px]"
+          ></textarea>
+          <div className="button flex gap-3">
+            <button
+              onClick={() => {
+                handleCopy();
+              }}
+              className="mt-[10px] border text-blue-300 border-blue-300 px-8 py-2 rounded-[5px] flex items-center "
+            >
+              <IoCopyOutline className="text-sm mr-1 text-blue-300" />
+              Copy Text
+            </button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
